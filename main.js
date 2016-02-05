df546e7d0a973638535d27032d7b2c4da8361016
Student <student@RU-016.local>
2016-02-04 17:53:16 -0700

    updated draft set

--- main.js
+++ main.js
@@ -3,19 +3,46 @@ angular.module('quoteApp', [])
 angular.module('quoteApp')
 	.factory('quoteFactory', [function(){
 
-		var Quotes = function( quote, author, source) {
+		var quoteArray = [];
+		var Quotes = function( quote, author, source, rating) {
 
 			this.quote = quote,
 			this.author = author,
-			this.source = source
+			this.source = source,
+			this.rating = rating,
+			quoteArray.push(this)
 		};
 
 		var quoteEntry = new Quotes (
 			"Disbelief in magic can force a poor soul into believing in government and business.",
 			"Tom Robbins",
-			"Even Cowgirls Get the Blues"
+			"Even Cowgirls Get the Blues",
+			2
 		);
-
+		var quoteEntry = new Quotes (
+			"Luck is what happens to you when fate gets tired of waiting.",
+			"Gregory David Roberts",
+			"Shantaram",
+			4
+		);
+		var quoteEntry = new Quotes (
+			"A real diamond is never perfect.",
+			"Anthony Doerr",
+			"All the Light We Cannot See",
+			5
+		);
+		var quoteEntry = new Quotes (
+			"A sense of humor...is superior to any religion so far devised.",
+			"Tom Robbins",
+			"Jitterbug Perfume",
+			5
+		);
+				var quoteEntry = new Quotes (
+			"The bicycle is a curious vehicle. Its passenger is its engine.",
+			"John Howard",
+			"interview",
+			1
+		);
 		var trueFalse = {
 			hideSubmitTab	: true,
 			hideListTab		: true,
@@ -23,7 +50,7 @@ angular.module('quoteApp')
 		};
 
 		return {
-			quoteList 	: quoteEntry,
+			quoteList 	: quoteArray,
 			trueFalse	: trueFalse
 		}
 
@@ -35,6 +62,5 @@ angular.module('quoteApp')
 		$scope.quoteList = quoteFactory.quoteList,
 		$scope.trueFalse = quoteFactory.trueFalse
 
-		$scope.greeting = 'hello'
 
 	}]);
